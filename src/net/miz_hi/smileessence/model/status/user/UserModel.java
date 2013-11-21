6ab521becd794164779f191f25c1f90f5768c4e7
laco0416 <laco0416@gmail.com>
2013-11-21 18:01:03 +0900

    アイコンのプリキャッシュをやめて起動時のアイコン表示を優先。

--- src/net/miz_hi/smileessence/model/status/user/UserModel.java
+++ src/net/miz_hi/smileessence/model/status/user/UserModel.java
@@ -1,7 +1,6 @@
 package net.miz_hi.smileessence.model.status.user;
 
 import net.miz_hi.smileessence.Client;
-import net.miz_hi.smileessence.cache.ImageCache;
 import net.miz_hi.smileessence.model.status.IStatusModel;
 import net.miz_hi.smileessence.preference.EnumPreferenceKey;
 import net.miz_hi.smileessence.status.EnumNameStyle;
@@ -58,7 +57,7 @@ public class UserModel implements IStatusModel
         favoriteCount = user.getFavouritesCount();
         createdAt = user.getCreatedAt();
         isProtected = user.isProtected();
-        ImageCache.preCache(iconUrl);
+        //        ImageCache.preCache(iconUrl);
         return this;
     }
 
