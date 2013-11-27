8ad0f6252c189fcf4f42f21631bbac2d18e1decd
laco0416 <laco0416@gmail.com>
2013-11-27 19:44:14 +0900

    Refactor TweetUtils

--- src/net/miz_hi/smileessence/status/TweetUtils.java
+++ src/net/miz_hi/smileessence/status/TweetUtils.java
@@ -2,14 +2,12 @@ package net.miz_hi.smileessence.status;
 
 import net.miz_hi.smileessence.Client;
 import net.miz_hi.smileessence.cache.TweetCache;
-import net.miz_hi.smileessence.core.MyExecutor;
 import net.miz_hi.smileessence.model.status.tweet.TweetModel;
-import net.miz_hi.smileessence.twitter.API;
+import net.miz_hi.smileessence.task.impl.ShowTweetTask;
 import net.miz_hi.smileessence.twitter.ResponseConverter;
 import twitter4j.Status;
 import twitter4j.UserMentionEntity;
 
-import java.util.concurrent.Callable;
 import java.util.concurrent.Future;
 
 public class TweetUtils
@@ -31,20 +29,12 @@ public class TweetUtils
         return false;
     }
 
-    public static TweetModel getOrCreateStatusModel(final long id)
+    public static TweetModel getOrCreateStatusModel(long id)
     {
         TweetModel statusModel = TweetCache.get(id);
         if (statusModel == null)
         {
-            Future<Status> f = MyExecutor.submit(new Callable<Status>()
-            {
-
-                @Override
-                public Status call() throws Exception
-                {
-                    return API.showStatus(Client.getMainAccount(), id);
-                }
-            });
+            Future<Status> f = new ShowTweetTask(id).callAsync();
             Status status;
             try
             {
