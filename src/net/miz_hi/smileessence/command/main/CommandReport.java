8dba3a86364d00f6b4ac2723bbd2d926fc088e02
laco0416 <laco0416@gmail.com>
2013-11-30 10:31:01 +0900

    Change Report format

--- src/net/miz_hi/smileessence/command/main/CommandReport.java
+++ src/net/miz_hi/smileessence/command/main/CommandReport.java
@@ -1,26 +1,29 @@
 package net.miz_hi.smileessence.command.main;
 
+import net.miz_hi.smileessence.Client;
+import net.miz_hi.smileessence.R;
 import net.miz_hi.smileessence.command.MenuCommand;
 import net.miz_hi.smileessence.system.PostSystem;
 
 public class CommandReport extends MenuCommand
 {
 
-	public CommandReport()
-	{
-	}
+    public CommandReport()
+    {
+    }
 
-	@Override
-	public String getName()
-	{
-		return "作者にレポートを送る";
-	}
+    @Override
+    public String getName()
+    {
+        return "作者にレポートを送る";
+    }
 
-	@Override
-	public void workOnUiThread()
-	{
-		PostSystem.setText("#SmileEssence @laco0416 ");
-		PostSystem.openPostPage();
-	}
+    @Override
+    public void workOnUiThread()
+    {
+        PostSystem.setText(" #SmileEssence @laco0416 [" + Client.getString(R.string.app_version) + "]");
+        PostSystem.setCursor(0);
+        PostSystem.openPostPage();
+    }
 
 }
