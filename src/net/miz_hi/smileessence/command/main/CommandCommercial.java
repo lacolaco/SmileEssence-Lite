d4876bc19252014cc3aab2c889172e1c7ebb50b4
laco0416 <laco0416@gmail.com>
2013-11-30 23:27:29 +0900

    Fix App Url

--- src/net/miz_hi/smileessence/command/main/CommandCommercial.java
+++ src/net/miz_hi/smileessence/command/main/CommandCommercial.java
@@ -1,23 +1,25 @@
 package net.miz_hi.smileessence.command.main;
 
+import net.miz_hi.smileessence.Client;
+import net.miz_hi.smileessence.R;
 import net.miz_hi.smileessence.command.MenuCommand;
 import net.miz_hi.smileessence.system.PostSystem;
 
 public class CommandCommercial extends MenuCommand
 {
 
-	@Override
-	public String getName()
-	{
-		return "宣伝する";
-	}
+    @Override
+    public String getName()
+    {
+        return "宣伝する";
+    }
 
-	@Override
-	public void workOnUiThread()
-	{
-		String str = "Android用Twitterクライアント「SmileEssence Lite」をチェック！\r\n http://smileessence.miz-hi.net/";
-		PostSystem.setText(str);
-		PostSystem.openPostPage();
-	}
+    @Override
+    public void workOnUiThread()
+    {
+        String str = "Android用Twitterクライアント「SmileEssence Lite」をチェック！\r\n " + Client.getString(R.string.app_url);
+        PostSystem.setText(str);
+        PostSystem.openPostPage();
+    }
 
 }
