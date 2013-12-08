ed6473682ba7f07381336825ac47f3aa8fd6fb84
laco0416 <laco0416@gmail.com>
2013-12-08 10:41:49 +0900

    Add new utils

--- src/net/miz_hi/smileessence/twitter/TwitterUtil.java
+++ src/net/miz_hi/smileessence/twitter/TwitterUtil.java
@@ -1,5 +1,7 @@
 package net.miz_hi.smileessence.twitter;
 
+import net.miz_hi.smileessence.model.status.tweet.TweetModel;
+
 public class TwitterUtil
 {
 
@@ -21,5 +23,15 @@ public class TwitterUtil
     public static String getTwilogURL(String screenName)
     {
         return "http://twilog.org/" + screenName;
+    }
+
+    public static String getTweetURL(TweetModel tweet)
+    {
+        return String.format("https://twitter.com/%s/status/%s", tweet.getOriginal().user.screenName, tweet.getOriginal().statusId);
+    }
+
+    public static String getSTOTString(TweetModel tweet)
+    {
+        return String.format("%s: %s [%s]", tweet.getOriginal().user.screenName, tweet.getText(), getTweetURL(tweet));
     }
 }
